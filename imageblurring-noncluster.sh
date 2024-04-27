5548153e69eb056f4e9dace53cbd65a1240e6f83
morri2wj <morri2wj@dukes.jmu.edu>
2024-04-27 14:05:19 -0400

    Add early noncluster run scripts

---
+++ imageblurring-noncluster.sh
@@ -0,0 +1,64 @@
+# set up environment
+
+if [ -d "RAJA-example" ]; then
+    # If the folder exists, delete it
+    rm -r "RAJA-example"
+fi
+
+mkdir RAJA-example
+
+# NOTE: only build RAJA if RAJA build dir not found.
+if [[ ! -d RAJA-build ]]; then
+  git clone --recursive https://github.com/LLNL/RAJA.git && mkdir RAJA-build && mkdir RAJA-install
+
+  echo 'Success in environment set up'
+
+  # build RAJA 
+  cd RAJA-build
+  # Disable additional cruft -- wastes time!
+  cmake -DRAJA_ENABLE_TESTS=Off -DRAJA_ENABLE_EXAMPLES=Off -DRAJA_ENABLE_EXERCISES=Off -DCMAKE_INSTALL_PREFIX=../RAJA-install -DENABLE_OPENMP=On ../RAJA
+  make -j && make install
+  echo 'Success in build'
+
+  cd ../
+fi
+
+# make and run program
+cp ./example/serial.cpp ./RAJA-example
+cp ./example/cuda.cu ./RAJA-example
+cp ./example/openmp.cpp ./RAJA-example
+cp ./example/pthread.cpp ./RAJA-example
+cp ./example/raja.cpp ./RAJA-example
+cp ./example/Makefile ./RAJA-example
+cd ./RAJA-example
+make
+
+../make_ppms.sh
+
+gcc -o dif_compare ../dif/dif.c
+
+echo 'Running program on wilson.jpg'
+for i in {1..5}; do
+    echo "Test $i"
+    ../run-noncluster.sh ../wilson.jpg
+done
+
+echo 'Running program on wilson2.jpg'
+for i in {1..5}; do
+    echo "Test $i"
+    ../run-noncluster.sh ../wilson2.jpg
+done
+
+echo 'Running program on duke_dog.jpg'
+for i in {1..5}; do
+    echo "Test $i"
+    ../run-noncluster.sh ../duke_dog.jpg
+done
+
+echo 'Running program on duke_dog2.jpg'
+for i in {1..5}; do
+    echo "Test $i"
+    ../run-noncluster.sh ../duke_dog2.jpg
+done
+
+echo 'Success in program run'
---
+++ run-noncluster.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#
+# To run on the cluster w/ a GPU node:
+#
+#   srun --gres=gpu ./run.sh <infile>
+#
+
+# Check for filename
+if [ $# -ne 1 ]; then
+    echo "Usage: ./run.sh <image>"
+    exit 1
+fi
+
+# Runs the ImageMagick 'identify' command to extract image dimensions
+dims=$(identify ${@: -1} | cut -f 3 -d ' ')
+width=$(echo ${dims} | cut -f 1 -d 'x')
+height=$(echo ${dims} | cut -f 2 -d 'x')
+
+# Display help message if no dimensions are found
+if [[ -z "$dims" ]]; then
+  help=true
+fi
+
+# Create a temporary file in PPM format
+temp_file="converted_image.ppm"
+convert ${@: -1} -compress None PPM:${temp_file}
+
+
+# Run serial program if it is present
+if [ -e "serial" ]; then
+    echo -n "Serial:       "
+    ./serial ${temp_file} output_serial.ppm ${width} ${height}
+    convert output_serial.ppm PNG:output_serial.png
+    #rm output_serial.ppm
+else
+    echo "ERROR: Serial program not found"
+fi
+
+if [ -e "pthread" ]; then
+    echo -n "Pthread:      "
+    ./pthread ${temp_file} output_pthread.ppm ${width} ${height} 16
+    convert output_pthread.ppm PNG:output_pthread.png
+    #rm output_pthread.ppm
+else
+    echo "ERROR: Pthread program not found"
+fi
+
+if [ -e "openmp" ]; then
+    echo -n "OpenMP:       "
+    ./openmp ${temp_file} output_openmp.ppm ${width} ${height}
+    convert output_openmp.ppm PNG:output_openmp.png
+    #rm output_openmp.ppm
+    
+else
+    echo "ERROR: OpenMP program not found"
+fi
+
+#Run CUDA program if it is present
+if [ -e "cuda" ]; then
+    echo -n "CUDA:         "
+    ./cuda ${temp_file} output_cuda.ppm ${width} ${height}
+    convert output_cuda.ppm PNG:output_cuda.png
+    #rm output_cuda.ppm
+else
+    echo "ERROR: CUDA program not found"
+fi
+
+
+if [ -e "raja" ]; then
+    echo -n "RAJA:         "
+    ./raja ${temp_file} output_raja.ppm ${width} ${height}
+    convert output_raja.ppm PNG:output_raja.png
+    #rm output_raja.ppm
+
+else
+    echo "ERROR: RAJA program not found"
+fi
+
+
+./dif_compare output_serial.png output_pthread.png
+./dif_compare output_serial.png output_openmp.png
+./dif_compare output_serial.png output_cuda.png
+./dif_compare output_serial.png output_raja.png
+
+
+# Remove the temporary file
+rm ${temp_file}
